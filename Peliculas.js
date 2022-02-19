a0eba888913dd9c2ab8180668352fd4c89bb07a3
Juliana Castillo <96964513+jlianacastillo@users.noreply.github.com>
2022-02-19 10:09:31 -0500

    Uso de switch

---
+++ Peliculas.js
@@ -0,0 +1,17 @@
+var pelicula = 707;
+
+switch (pelicula) {
+  case 707:
+    console.log("Soy pelicula 707!");
+    break;
+  case 10:
+    console.log("Soy caso 10!");
+    break;
+  case 100:
+    console.log("Soy caso 100!");
+    break;
+   default: 
+    console.log("No soy ninguna pelicula ni ningun caso");
+    }
+    
+    //Se debe de mostrar en consola Soy pelicula 707!
